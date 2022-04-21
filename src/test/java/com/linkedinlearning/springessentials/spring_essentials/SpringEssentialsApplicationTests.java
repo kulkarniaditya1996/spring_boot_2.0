d97593d63a6447c5b1d2093e64ee4f0177e76248
Aditya <kulkarniaditya1996@gmail.com>
2022-04-20 20:24:34 -0700

    first commit

---
+++ src/test/java/com/linkedinlearning/springessentials/spring_essentials/SpringEssentialsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.linkedinlearning.springessentials.spring_essentials;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringEssentialsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
